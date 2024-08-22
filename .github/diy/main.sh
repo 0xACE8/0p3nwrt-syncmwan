beffb6f68e15e23c119ea95d61d8b9e6f90fe91c
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2024-08-22 00:08:13 -0700

    Update main.sh

--- .github/diy/main.sh
+++ .github/diy/main.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
-function git_clone() {
-  git clone --depth 1 $1 $2 || true
- }
-function git_sparse_clone() {
-  branch="$1" rurl="$2" localdir="$3" && shift 3
-  git clone -b $branch --depth 1 --filter=blob:none --sparse $rurl $localdir
-  cd $localdir
-  git sparse-checkout init --cone
-  git sparse-checkout set $@
-  mv -n $@ ../
-  cd ..
-  rm -rf $localdir
-  }
-function mvdir() {
-mv -n `find $1/* -maxdepth 0 -type d` ./
-rm -rf $1
+
+function merge_package() {
+    # 参数1是分支名,参数2是库地址,参数3是所有文件下载到指定路径。
+    # 同一个仓库下载多个文件夹直接在后面跟文件名或路径，空格分开。
+    if [[ $# -lt 3 ]]; then
+        echo "Syntax error: [$#] [$*]" >&2
+        return 1
+    fi
+    trap 'rm -rf "$tmpdir"' EXIT
+    branch="$1" curl="$2" target_dir="$3" && shift 3
+    rootdir="$PWD"
+    localdir="$target_dir"
+    [ -d "$localdir" ] || mkdir -p "$localdir"
+    tmpdir="$(mktemp -d)" || exit 1
+    git clone -b "$branch" --depth 1 --filter=blob:none --sparse "$curl" "$tmpdir"
+    cd "$tmpdir"
+    git sparse-checkout init --cone
+    git sparse-checkout set "$@"
+    # 使用循环逐个移动文件夹
+    for folder in "$@"; do
+        mv -f "$folder" "$rootdir/$localdir"
+    done
+    cd "$rootdir"
 }
-git clone --depth 1 https://github.com/x-wrt/packages/net/mwan3 mwan3
-git clone --depth 1 https://github.com/x-wrt/luci/applications/luci-app-mwan3 luci-app-mwan3
-git clone --depth 1 https://github.com/kiddin9/openwrt-packages/luci-app-syncdial luci-app-syncdial
+
+merge_package master https://github.com/x-wrt/packages mwan3 net/mwan3
+merge_package master https://github.com/x-wrt/luci luci-app-mwan3 applications/luci-app-mwan3
+merge_package master https://github.com/kiddin9/openwrt-packages luci-app-syncdial luci-app-syncdial
+#merge_package main https://github.com/shiyu1314/openwrt-onecloud target/linux kernel/6.6/amlogic
 
 exit 0
