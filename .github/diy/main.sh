23021cc82d3d363142ac681b89def15f02d9fb26
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2024-08-21 23:52:07 -0700

    Create main.sh

---
+++ .github/diy/main.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+function git_clone() {
+  git clone --depth 1 $1 $2 || true
+ }
+function git_sparse_clone() {
+  branch="$1" rurl="$2" localdir="$3" && shift 3
+  git clone -b $branch --depth 1 --filter=blob:none --sparse $rurl $localdir
+  cd $localdir
+  git sparse-checkout init --cone
+  git sparse-checkout set $@
+  mv -n $@ ../
+  cd ..
+  rm -rf $localdir
+  }
+function mvdir() {
+mv -n `find $1/* -maxdepth 0 -type d` ./
+rm -rf $1
+}
+git clone --depth 1 https://github.com/x-wrt/packages/net/mwan3
+git clone --depth 1 https://github.com/x-wrt/luci/applications/luci-app-mwan3
+git clone --depth 1 https://github.com/kiddin9/openwrt-packages/luci-app-syncdial
+
+exit 0
